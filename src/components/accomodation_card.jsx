4798e6ae7134cf0aa4b778ca699dddb2fc20462a
BrigitteLPB <lpb.brigitte@gmail.com>
2023-09-19 21:36:43 +0200

    remove debug feature

--- src/components/accomodation_card.jsx
+++ src/components/accomodation_card.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 import "style/components/accomodation_card.scss"
 
-function AccomodationCard({logement_data, id}){
+function AccomodationCard({logement_data}){
 	return (
-		<Link to={`/fiche-logement/${logement_data.id}`} key={`accomodation-item-${logement_data.id}-${id}`} className="accomodation-item-wrapper">
+		<Link to={`/fiche-logement/${logement_data.id}`} key={`accomodation-item-${logement_data.id}`} className="accomodation-item-wrapper">
 			<img src={logement_data.cover} alt=""/>
 			<span className="img-black-filter"/>
 			<h2 className="text-normal accomodation-title">{logement_data["title"]}</h2>
