9006d459f230695230249a5b5b0da0b5bd28aa19
BrigitteLPB <lpb.brigitte@gmail.com>
2023-09-29 10:29:36 +0200

    add && condition in carousel

--- src/components/common/footer.jsx
+++ src/components/common/footer.jsx
@@ -1,5 +1,5 @@
 import "style/components/common/footer.scss";
-import Logo from "components/logo";
+import Logo from "components/common/logo";
 
 function Footer(){
 	return (
--- src/components/common/header.jsx
+++ src/components/common/header.jsx
@@ -1,5 +1,5 @@
 import { Link, useLocation } from "react-router-dom"
-import Logo from "components/logo";
+import Logo from "components/common/logo";
 
 import "style/components/common/header.scss"
 
