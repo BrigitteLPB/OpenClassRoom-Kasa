e5368b4ab930978fbf6a5dd8ad10fe4263ca3910
BrigitteLPB <lpb.brigitte@gmail.com>
2023-10-07 10:45:56 +0200

    refacto of about page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -2,6 +2,7 @@ import Collapse from "components/common/collapse"
 
 import image from "assets/kalen-emsley-Bkci_8qcdvQ.png"
 import "style/pages/about.scss"
+import about_texts from 'assets/about-texts.json';
 
 function About() {
 	return (
@@ -11,18 +12,18 @@ function About() {
 				<span className="img-over"/>
 			</div>
 			<div className="collapse-wrapper">
-				<Collapse title="Fiabilité">
-					<p>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes.</p>
-				</Collapse>
-				<Collapse title="Respect">
-					<p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-				</Collapse>
-				<Collapse title="Service">
-					<p>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
-				</Collapse>
-				<Collapse title="Sécurité">
-					<p>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
-				</Collapse>
+				{
+					about_texts.map((data) => {
+						if ("name" in data && "text" in data){
+							return (
+								<Collapse title={data["name"]}>
+									<p>{data["text"]}</p>
+								</Collapse>
+							)
+						}
+						return null;
+					})
+				}
 			</div>
 		</div>
 	);
